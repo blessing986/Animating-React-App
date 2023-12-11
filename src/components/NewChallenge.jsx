c39302a850fa664a070fb6106d29d790546c7169
Blessing U <blessing986@users.noreply.github.com>
2023-12-11 08:25:18 +0000

    chore: animating staggered lists of images

--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -57,7 +57,13 @@ export default function NewChallenge({ onDone }) {
           <input ref={deadline} type="date" name="deadline" id="deadline" />
         </p>
 
-        <ul id="new-challenge-images">
+        <motion.ul
+          id="new-challenge-images"
+          variants={{
+            // staggerChildren controls the delay by which the child element(li element) will start their animation
+            visible: { transition: { staggerChildren: 0.05 } }, 
+          }}
+        >
           {images.map((image) => (
             <motion.li
               variants={{
@@ -73,7 +79,7 @@ export default function NewChallenge({ onDone }) {
               <img {...image} />
             </motion.li>
           ))}
-        </ul>
+        </motion.ul>
 
         <p className="new-challenge-actions">
           <button type="button" onClick={onDone}>
